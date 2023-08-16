22cfd07fc9802e63847209842f2f5e49e93cd273
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-08-16 13:35:21 +0700

    hw04-6

--- Homework/HW04/hw04-6-man-hour.cpp
+++ Homework/HW04/hw04-6-man-hour.cpp
@@ -1,6 +1,6 @@
 /*
     เขียนโปรแกรมภาษาซีเพื่อรับข้อมูลพนักงานของบริษัทซอร์ฟแวร์ โดยรับข้อมูล รหัสประจำตัวพนักงาน , จำนวนชั่วโมงที่ทำงาน , รายได้ต่อชั่วโมง จากนั้นให้แสดงข้อมูลเลขประจำตัวพนักงาน พร้อมกับรายได้ทั้งหมดที่หนักงานจะได้รับทั้งหมด
-    (ผลลัพธ์ของ Salary จะต้องอยู่ในรูปแบบ Decimal Number เท่านั้น เช่น 374,000)
+    (ผลลัพธ์ของ Salary จะต้องอยู่ในรูปแบบ Decimal Number เท่านั้น เช่น 374,000.00)
 
     Test case:
         Input the Employees ID(Max. 10 chars): 
@@ -27,26 +27,86 @@
         Salary = U$ 374,000.00
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    char EmployeeID[10];
-    float hours;
-    float salaryPerHour;
-    float totalSalary;
+    char employeeId[10] ;
+    float hoursWorked, hourlyRate ;
 
-    printf("Input the Employees ID(Max. 10 chars): \n") ;
-    scanf("%s", EmployeeID);
+    printf( "Input the Employees ID(Max. 10 chars): \n" ) ;
+    scanf( "%s", &employeeId ) ;
+    printf( "Input the working hrs: \n" ) ;
+    scanf( "%f", &hoursWorked ) ;
+    printf( "Salary amount/hr: \n" ) ;
+    scanf( "%f", &hourlyRate ) ;
 
-    printf("Input the working hrs: \n") ;
-    scanf("%f", &hours) ;
- 
-    printf("Salary amount/hr: \n") ;
-    scanf("%f", &salaryPerHour) ;
+    float salary = hoursWorked * hourlyRate;
+    printf( "Expected Output: \n" ) ;
+    printf( "Employee ID = %s\n", employeeId ) ;
+    printf( "Salary = U$ " ) ;
 
-    totalSalary = salaryPerHour * hours ;
+    char num[100];
+    sprintf( num, "%.2f", salary ) ;
 
-    printf("Employee ID: %s\n", EmployeeID) ;
-    printf("Salary: U$ %.2f",totalSalary) ;
+    int size = strlen( num ) ;
+    int zise = 0 ;
+    int count = 0, dot = 0, x = 0 ;
 
-    return 0;
-}
+    for ( x = 0 ; x < size ; x++ ) {
+        if (num[x] == '.') {
+            dot = x ;
+            while ( count < size - x - 1 ) {
+                count++; 
+            } // end while
+        } // end if
+    } // end for
+    if (count > 0) {
+        zise = size - count ;
+        zise-- ;
+    } else {
+        zise = size ; 
+    } //end else
+    if (zise < 4) {
+        for ( x = 0 ; x < zise ; x++ ) {
+            printf( "%c", num[x] ) ;
+        } // end for
+    } // end if
+    if ( zise > 3 ) {
+        if ( zise % 3 == 1 ) {
+            for ( x = 0 ; x < zise ; x++ ) {
+                printf( "%c", num[x] ) ;
+                if ( ( zise - x ) % 3 == 1 && x != zise - 1 ) {
+                    printf( "," ) ;
+                } // end if
+            } // end for
+        } // end if
+        if ( zise % 3 == 2 ) {
+            for ( x = 0 ; x < zise ; x++ ) {
+                printf( "%c", num[x] ) ;
+                if ( ( zise - x ) % 3 == 1 && x != zise - 1 ) {
+                    printf( "," ) ;
+                } // end if
+            } // end for
+        } // end if
+        if ( zise % 3 == 0 ) {
+            for ( x = 0 ; x < zise ; x++ ) {
+                printf( "%c", num[x] ) ;
+                if ( ( zise - x ) % 3 == 1 && x != zise - 1 ) {
+                    printf( "," ) ;
+                } // end if
+            } // end for
+        } // end if
+    } // end if
+    if ( num[dot] == '.' ) {
+        if ( num[dot + 4] >= 53 && num[dot + 4] <= 58 ) {
+            num[dot + 3]++;
+        } // end if 
+    } // end if 
+    printf( "." ) ;
+    for ( x = dot + 1 ; x <= dot + 2 && x < size ; x++ ) {
+        printf( "%c", num[x] ) ;
+    } // end for 
+    printf( "\n" ) ;
+    return 0 ;
+} // end main function
